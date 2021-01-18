57fcfb932d2a09f6bcddc32d6acbdc5aff3069f7
dmitriikorol <67783127+DimitriiKorol@users.noreply.github.com>
2021-01-19 00:46:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+define("ROOT", $_SERVER['DOCUMENT_ROOT']);
+define("IMG_SMALL", ROOT . '/img/small/');
+define("IMG_BIG", ROOT . '/img/big/');
+
+include "db.php";
+$imgarray = mysqli_query($db, "SELECT name FROM images");
+var_dump(mysqli_query($db, "SELECT name FROM `images`"));
+
+//$imgarray = array_splice(scandir('img/small/'), 2);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Gallery</title>
+</head>
+<body>
+  <div class="content">
+    <h2 class="title-h2">Gallery</h2>
+    <div class="container">
+
+<? foreach ($imgarray as $name): ?>
+<div class="item"><img src="img/large/<?=$name?>" alt="" class="big"><img src="img/small/<?=$name?>" alt=""></div>
+<? endforeach; ?>
+
+    </div>
+    <p>Upload images:</p>
+    <form class="" method="post" enctype="multipart/form-data">
+      <input type="file" name="image">
+      <input type="submit" name="load" value="Upload">
+    </form>
+  </div>
+
+</body>
+</html>
