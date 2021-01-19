1190ed4ba2418abb44e8355e196997372f65f7aa
dmitriikorol <67783127+DimitriiKorol@users.noreply.github.com>
2021-01-19 14:49:26 +0700

    Update image.php

--- image.php
+++ image.php
@@ -5,8 +5,13 @@ define("IMG_BIG", ROOT . '/img/big/');
 
 include "db.php";
 
-$imgarray = mysqli_query($db, "SELECT * FROM images");
+$id = (int)$_GET['id']; 
 
+$result = mysqli_query($db, "SELECT * FROM images WHERE id = {$id}");
+$imgres = mysqli_fetch_assoc($result);
+$messg = "";
+if($result -> num_rows != 0) $imgres = mysqli_fetch_assoc($result);
+else  $messg = "No file found in db";
 ?> 
 
 <!DOCTYPE html>
@@ -19,13 +24,15 @@ $imgarray = mysqli_query($db, "SELECT * FROM images");
 </head>
 <body>
   <div class="content">
+    <a href="/">Главная</a>
     <h2 class="title-h2">Gallery</h2>
     <div class="container">
 
-
-<div class="item"><img src="img/big/<?=$name['name']?>" alt="pic"></div>
-
-
+<? if(empty($messg)): ?>
+<div class="item"><img src="img/big/<?=$imgres['name']?>" alt="pic"></div>
+<? else: ?>
+      <p style="color: red;"><?= $messg ?></p>
+<? endif; ?>
     </div>
     <p>Upload images:</p>
     <form class="" method="post" enctype="multipart/form-data">
